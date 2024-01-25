93db45e0314a302814a7ff7f5d607bdc1a992092
Adam Narozniak <narozniak.adam4@gmail.com>
2024-01-25 18:42:52 +0100

    Remove redundant parameter from create_graphs (rename too)

--- src/network.py
+++ src/network.py
@@ -1,10 +1,12 @@
 import networkx as nx
 
-# Function to create a graph with latency functions using the modified matrices
-def create_graph_with_latency_modified(n, a_matrix, b_matrix, c_matrix):
+
+def create_graph_from_matrices(a_matrix, b_matrix, c_matrix):
+    n = len(a_matrix)
     G = nx.DiGraph()
     for i in range(n):
         for j in range(n):
-            if a_matrix[i][j] != 0 or b_matrix[i][j] != 0 or c_matrix[i][j] != 0:  # Check if an edge exists
+            # Check if an edge exists
+            if a_matrix[i][j] != 0 or b_matrix[i][j] != 0 or c_matrix[i][j] != 0:
                 G.add_edge(i, j, a=a_matrix[i][j], b=b_matrix[i][j], c=c_matrix[i][j])
     return G
