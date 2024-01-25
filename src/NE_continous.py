b0c12cb0deffe568dc6521ff44dba29f4607ea55
Adam Narozniak <narozniak.adam4@gmail.com>
2024-01-25 18:42:15 +0100

    Add default values to calculate_nash_flow

--- src/NE_continous.py
+++ src/NE_continous.py
@@ -1,11 +1,14 @@
-from NE_discrete import all_paths_without_loops, update_edge_flow, calculate_all_latencies
-import copy
+from NE_discrete import all_paths_without_loops, update_edge_flow, \
+    calculate_all_latencies
+
 
 def init_partition(G, paths):
     return [1 / len(paths)] * len(paths)
 
+
 MIN_DELTA = 1e-6
 STOPPING_CONDITION = 1e-7
+
 
 def compare_latencies(latency, partition):
     non_zero_latencies = []
@@ -16,23 +19,26 @@ def compare_latencies(latency, partition):
     difference = max(non_zero_latencies) - min(non_zero_latencies)
     return difference
 
-    
 
-
-def calculate_nash_flow(G, start_node, stop_node, max_iter=100000, delta = 0.5, lr = 1 - 1e-3, eps=1e-6, verbose=False):
+def calculate_nash_flow(G, start_node=None, stop_node=None, max_iter=100000, delta=0.5,
+                        lr=1 - 1e-3, eps=1e-6, verbose=False):
     """
     Calculate the Nash flow of a given graph.
     """
     # Initialize the flow of all edges to 0
+    if start_node is None:
+        start_node = 0
+    if stop_node is None:
+        stop_node = len(G) - 1
     for edge in G.edges:
         G[edge[0]][edge[1]]["flow"] = 0
-    
+
     # Find all paths without loops from start_node to stop_node
     paths = all_paths_without_loops(G, start_node, stop_node)
-    
+
     # Initialize the partition
     partition = init_partition(G, paths)
-    
+
     # Update the flow of the edges in a given path in a graph
     update_edge_flow(G, paths, partition)
     latencies = None
@@ -41,7 +47,7 @@ def calculate_nash_flow(G, start_node, stop_node, max_iter=100000, delta = 0.5, 
         for i in range(len(paths)):
             # Calculate the latencies of all paths in a graph
             latencies = calculate_all_latencies(G, paths)
-                
+
             min_latency = min(latencies)
             min_index = latencies.index(min_latency)
 
@@ -51,7 +57,7 @@ def calculate_nash_flow(G, start_node, stop_node, max_iter=100000, delta = 0.5, 
 
             # Update the partition
             if partition[i] != 0:
-                diff = partition[i] * delta 
+                diff = partition[i] * delta
                 partition[i] -= diff
                 partition[i] = 0 if partition[i] < eps else partition[i]
                 partition[min_index] += diff
@@ -82,5 +88,3 @@ def calculate_nash_flow(G, start_node, stop_node, max_iter=100000, delta = 0.5, 
     print("Final partition: ", partition)
 
     return G, partition, paths, final_latencies
-    
-     
